e38bfd74e6acab9439b780b739d0128f2b445cc4
sebba <sebba@op65n.org>
2022-02-19 13:38:01 +0100

    added onDisable to unregister everything on shutdown

--- src/main/java/org/op65n/translate/Translate.java
+++ src/main/java/org/op65n/translate/Translate.java
@@ -1,5 +1,7 @@
 package org.op65n.translate;
 
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.PluginManager;
 import org.op65n.translate.command.CommandReload;
@@ -9,11 +11,16 @@ import org.op65n.translate.impl.AzureTranslate;
 import org.op65n.translate.listener.ChatListener;
 import org.op65n.translate.model.Translator;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Translate extends Plugin {
 
     private static Translate self = null;
+
+    private final Set<Listener> listeners = new HashSet<>();
+    private final Set<Command> commands = new HashSet<>();
 
     public static Translate self() {
         Objects.requireNonNull(self, String.format("Instance of %s is not yet available", Translate.class.getSimpleName()));
@@ -29,10 +36,26 @@ public class Translate extends Plugin {
         final PluginManager manager = this.getProxy().getPluginManager();
         final Translator translator = new AzureTranslate();
 
-        manager.registerListener(this, new ChatListener(translator));
-        manager.registerCommand(this, new CommandTranslateToggle());
-        manager.registerCommand(this, new CommandTranslate(translator));
-        manager.registerCommand(this, new CommandReload());
+        listeners.add(new ChatListener(translator));
+
+        commands.add(new CommandTranslateToggle());
+        commands.add(new CommandTranslate(translator));
+        commands.add(new CommandReload());
+
+        listeners.forEach(listener -> manager.registerListener(this, listener));
+        commands.forEach(command -> manager.registerCommand(this, command));
     }
 
+    /**
+     * Called when this plugin is disabled.
+     */
+    @Override
+    public void onDisable() {
+        super.onDisable();
+
+        final PluginManager manager = this.getProxy().getPluginManager();
+
+        listeners.forEach(manager::unregisterListener);
+        commands.forEach(manager::unregisterCommand);
+    }
 }
